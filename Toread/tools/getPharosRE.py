5c3396e102ebcf5118586fe2b1964ff9742a4453
wanggd04 <wanggd04@d2cde47a-9558-11de-a4d2-b33cf26bcc48>
2009-12-25 07:28:11 +0000

    add a ugly tool about caculating the pharos RE we can use this when we want to evaluating Pharos

---
+++ Toread/tools/getPharosRE.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python
+
+
+import socket
+import sys
+sys.path.append('..')
+import pickle
+import math
+import time
+from Pharos import PHAROS
+from config import *
+
+
+
+PORT=NCPORT
+BUFLEN = 2048
+
+def getNodeNCObj( hostname, token ):
+	sockfd = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	ip = socket.gethostbyname(hostname)
+	sockfd.settimeout(10)
+	try:
+		sockfd.connect((ip,PORT))
+		sockfd.send(token)
+		ncbuf = sockfd.recv(BUFLEN)
+		sockfd.close()
+		ncobj = pickle.loads(ncbuf)
+		return ncobj
+	except:
+		print "Error when connecting to ", hostname
+		sockfd.close()
+		return None
+
+
+def loadHost(hostfile=""):
+	if (len(hostfile)==0):
+		return None;
+	hostlist=[]
+	fd = open(hostfile,'r')
+	lines = fd.readlines()
+	fd.close()
+	for line in lines:
+		h = line.split()[0]
+		if(len(h)):
+			hostlist.append(h)
+	return hostlist
+
+def saveResult( outputfile = "", REdict = {}):
+	if(len(outputfile)==0):
+		return False
+	fd = open(outputfile, 'w')
+	for k in REdict.keys():
+		fd.write( k + "    " + str(REdict[k]) + "\n" )
+	fd.close()
+	return True
+
+def saveLocalErr( outfile = "" , globalerrDict = {}, globalncDict={}, clustererrDict = {}, clusterncDict={}, hostidDict = {}):
+	if(len(outfile)==0):
+		return False
+	fd = open( outfile, 'w')
+	for k in globalerrDict.keys():
+		fd.write("[ClusterID]	" + k + "	" + hostidDict[k] +"\n")
+		fd.write( "[Golbal NC and Error]	" + k + "    " + str(globalncDict[k]) + "    " + str(globalerrDict[k]) + "\n")
+		fd.write( "[Cluster NC and Error]	" + k + "    " + str(clusterncDict[k]) + "    " + str(clustererrDict[k]) + "\n")
+	fd.close()
+	return True
+	
+
+def tcpPing(hostname):
+	pround = 5 # ping 5 times and use the average rtt
+	port = PINGPORT
+	ip = socket.gethostbyname( hostname )
+	rtt = 0
+	for i in range(pround):
+		try:
+			sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+			sock.settimeout(10)
+			sock.connect((ip,port))
+			rtt = rtt - time.time()
+			sock.send("this is a tcp ping test!\n")
+			buf = sock.recv(1024)
+			rtt = rtt + time.time()
+			sock.close()
+		except:
+			print "Error when ping host: ", hostname
+			return -100
+	rtt = rtt / pround
+	return rtt * 1000  # time.time() returns the seconds, here we need micro-seconds
+	
+
+
+def calDistance( nc1=[], nc2=[], Height=False):  # Height means whether we use height vector
+	if(len(nc1)!=len(nc2)):
+		print "calculating the distance is wrong!"
+		return -100;
+	l = len(nc1)
+	d = 0
+	for i in range(l-1):
+		d = d + ( nc1[i] - nc2[i] )**2
+	d = math.sqrt(d)
+	if(Height):
+		return d + nc1[l-1] + nc2[l-1]
+	else:
+		return d
+
+
+if (__name__=="__main__"):
+	msg="PharosInfo"
+	myselfname = socket.gethostname()
+
+	#inputfile = "../../nodelist.txt"
+	inputfile = "../../pharosDebuglist.txt"
+	out_re_file = "../evaluationResult/pharosRE.txt"
+	out_err_file = "../evaluationResult/pharosErr.txt"
+	usingHeight = True
+
+	# get the nc of myself
+	mync = getNodeNCObj( myselfname, msg )
+	if(mync==None):
+		print "can not get NC of myself!"
+		sys.exit(1)
+	myglobalvec = mync.globalvec
+	myglobalvec.append(mync.globalheight)
+	myglobalerr = mync.globalerror
+	myclustervec = mync.clustervec
+	myclustervec.append(mync.clusterheight)
+	myclustererr = mync.clustererror
+	myclusterid = mync.clusterID
+	
+
+	hosts = loadHost( inputfile )
+
+	hostGlobalNC = {}
+	hostClusterNC = {}
+	hostGlobalErr = {}
+	hostClusterErr = {}
+	hostID = {}
+
+	hostGlobalNC[myselfname] = myglobalvec
+	hostGlobalErr[myselfname] = myglobalerr
+	hostClusterErr[myselfname] = myclustererr
+	hostClusterNC[myselfname] = myclustervec
+	hostID[myselfname] = myclusterid
+	REdict = {}
+	
+	refreshMe = 5
+	count = 0
+
+	for host in hosts:
+		if count==refreshMe:
+			count = 0
+			# get the nc of myself
+			mync = getNodeNCObj( myselfname, msg )
+			if(mync==None):
+				print "can not get NC of myself!"
+				sys.exit(1)
+			print "Refreshing the information of myself..."
+			myglobalvec = mync.globalvec
+			myglobalvec.append(mync.globalheight)
+			myglobalerr = mync.globalerror
+			myclustervec = mync.clustervec
+			myclustervec.append(mync.clusterheight)
+			myclustererr = mync.clustererror
+			myclusterid = mync.clusterID	
+			hostGlobalNC[myselfname] = myglobalvec
+			hostGlobalErr[myselfname] = myglobalerr
+			hostClusterErr[myselfname] = myclustererr
+			hostClusterNC[myselfname] = myclustervec
+			hostID[myselfname] = myclusterid						
+		if(host==myselfname):
+			continue
+		ncobj = getNodeNCObj( host, msg )
+		count = count + 1
+		if ncobj==None:
+			continue
+		ncobj.globalvec.append(ncobj.globalheight)
+		ncobj.clustervec.append(ncobj.clusterheight)
+		hostGlobalNC[host] = 	ncobj.globalvec
+		hostGlobalErr[host] = ncobj.globalerror
+		hostClusterErr[host] = ncobj.clustererror
+		hostClusterNC[host] = ncobj.clustervec
+		hostID[host] = ncobj.clusterID
+		
+		# calculate the RE
+		k = myselfname + "--->" + host
+		rtt = tcpPing(host)
+		if (rtt == -100):
+			print "ping ",host," Error!"
+			continue
+		if hostID[host] == hostID[myselfname]:
+			predicted_rtt = calDistance( hostClusterNC[myselfname], hostClusterNC[host] , PHAROS_USING_HEIGHT_LOCAL)
+		else:
+			predicted_rtt = calDistance( hostGlobalNC[myselfname], hostGlobalNC[host] , PHAROS_USING_HEIGHT_GLOBAL)
+		re = abs(rtt-predicted_rtt)/min(rtt,predicted_rtt)
+		REdict[k] = re
+		print "RE result:",k,"=",re
+
+#		print "hostname: ", host
+#		print "host ip:", ncobj.ip
+#		print "host nc(including height):", ncobj.vec
+#		print "host error:", ncobj.error
+
+	r = saveResult( out_re_file, REdict )
+	if(not r):
+		print "save RE result fails!"
+	r = saveLocalErr( out_err_file, hostGlobalErr, hostGlobalNC , hostClusterErr, hostClusterNC, hostID)
+	if(not r):
+		print "save node NC result fails!"
+	
+	
