fad0715eb3d91f2e9fb3c7b8d99b9d8a0e5221b2
emmdim <savemanos@gmail.com>
2016-08-26 03:14:13 +0200

    Adds function to choose best proxy if there is one and other small changes

--- Toread/RoutingTable.py
+++ Toread/RoutingTable.py
@@ -5,19 +5,22 @@ import operator
 class Route(object):
 	def __init__(self,ip,distance,proxy=False, ttfb=0, myProxy=False):
 		self.ip = ip
-		self.distance = distance
-		self.ttfb = ttfb
+		self.distance = float(distance)/1000.0
+		self.ttfb = float(ttfb)
 		self.proxy = proxy
 		self.myProxy = myProxy
+		self.total = self.distance + self.ttfb
 	
 	def updateDistance(self,distance):
-		self.distance = distance
+		self.distance =float(distance)/1000.0
+		self.total = self.distance + self.ttfb
 	
 	def getDistance(self):
 		return self.distance
 	
 	def updateTTFB(self,ttfb):
-		self.ttfb = ttfb
+		self.ttfb = float(ttfb)
+		self.total = self.distance + self.ttfb
 	
 	def getTTFB(self):
 		return self.ttfb
@@ -32,7 +35,7 @@ class Route(object):
 		return self.proxy
 
 	def __str__(self):
-		return "{0}\t{1}\t{2}\t{3}\t{4}\n".format(self.ip,self.distance,self.proxy,self.ttfb,self.myProxy)
+		return "{0}\t{1}\t{2}\t{3}\t{4}\n".format(self.ip,self.distance,self.ttfb, self.total, self.proxy,self.myProxy)
 
 
 		
@@ -51,18 +54,26 @@ class RoutingTable(object):
 	def updateDistance(self, ip , distance):
 		self.routes[ip].updateDistance(distance)
 
-	def selectProxy(self,ip):
+	def chooseAsProxy(self,ip):
 		self.routes[self.proxy].unmakeMyProxy()
 		self.routes[ip].makeMyProxy()
+		self.proxy = ip
 	
 	def updateTTFB(self,ip,ttfb):
 		self.routes[ip].updateTTFB(ttfb)
 	
 
+	def chooseBestProxy(self):
+		routes = self.routes.values()
+		proxy_routes = [r for r in routes if r.proxy]
+		if proxy_routes:
+			min_route = min(proxy_routes,key=operator.attrgetter('total'))
+			self.chooseAsProxy(min_route.ip)
+
 	def store(self):
 		with open(VIVALDI_ROUTE_TABLE,'wb') as f:
-			f.write("ip\tdistance\tproxy\tTTFB\tmyProxy\n")
-			for r in sorted(self.routes.values(),key=operator.attrgetter('distance')):
+			f.write("ip\test_rtt\TTFB\tTotal\tproxy\ttotal\tmyProxy\n")
+			for r in sorted(self.routes.values(),key=operator.attrgetter('total')):
 				f.write(r.__str__())
 				#print r.__str__()
 	
