0634fb695836a4104d09fcb9097b613b1f10c503
emmdim <savemanos@gmail.com>
2016-08-31 02:29:47 +0200

    Part of the previous commit

---
+++ Toread/queryProxy.py
@@ -0,0 +1,13 @@
+from subprocess import PIPE, Popen
+import shlex
+
+
+def get_cmd(proxy):
+        cmd='curl -x '+proxy+':3128 -U '+USER+':\"'+PASS+'\" -m 180 -w %{time_starttransfer} www.google.com -o /dev/nul
+l -s'
+        return cmd
+
+def queryProxy(proxy):
+	command = Popen(shlex.split(get_cmd2(PROXY)),stdout=PIPE, stderr=PIPE)
+	return command
+
