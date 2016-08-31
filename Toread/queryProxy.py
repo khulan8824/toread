ff01f9d4f46f617764569bc59ec135a96a8b2f8b
emmdim <savemanos@gmail.com>
2016-08-31 02:51:17 +0200

    Fixes Bug

--- Toread/queryProxy.py
+++ Toread/queryProxy.py
@@ -1,6 +1,8 @@
 from subprocess import PIPE, Popen
 import shlex
 
+USER = 'david.pinilla'
+PASS = r'|Jn 5DJ\\7inbNniK|m@^ja&>C'
 
 def get_cmd(proxy):
         cmd='curl -x '+proxy+':3128 -U '+USER+':\"'+PASS+'\" -m 180 -w %{time_starttransfer} www.google.com -o /dev/null -s'
