4b8a2012edc4f2565ecab6f19586d5194c009d90
emmdim <savemanos@gmail.com>
2016-09-01 03:47:03 +0200

    Fixes Bug

--- Toread/Vivaldi/Vivaldi.py
+++ Toread/Vivaldi/Vivaldi.py
@@ -107,13 +107,11 @@ class Vivaldi():
 	neigh = self.myMananger.getNeighbor(MONITORED_CLIENT)
 	if neigh:
 	    with open('monitored_client','a') as f:
-	        neigh = self.myMananger.getNeighbor(MONITORED_CLIENT)
 	        distance = self.myClient.getCoor().getDistance(neigh.client.getCoor())
 	        f.write("{},{},{},{}\n".format(self.round,neigh.getRTT_last(),distance,neigh.getError()))
-	neigh = self.proxiesMananger.getNeighbor(MONITORED_PROXY)
+	neigh = self.proxiesManager.getNeighbor(MONITORED_PROXY)
 	if neigh:
 	    with open('monitored_proxy','a') as f:
-	        neigh = self.proxiesMananger.getNeighbor(MONITORED_PROXY)
 	        distance = self.myClient.getCoor().getDistance(neigh.client.getCoor())
 		f.write("{},{},{},{}\n".format(self.round,neigh.getRTT_last(),distance,neigh.getError()))
 	if not ME_PROXY:
