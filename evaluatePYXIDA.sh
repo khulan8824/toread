1d4e30adc6423e4d15699a199da55648d83d7561
Manos <savemanos@gmail.com>
2017-03-16 01:21:16 +0100

    Removing unecessary files

--- evaluatePYXIDA.sh
+++
@@ -1,1 +0,0 @@
-cd /home/goettingen_wgd/sandbox/Toread/tools&&python pyxidaGetRE.py>tmplog 2>&1&
--- getData.sh
+++
@@ -1,3 +0,0 @@
-cd /home/goettingen_wgd/sandbox/Toread/tools&&python getMyPharosNC.py>pnclog 2>&1&
-cd /home/goettingen_wgd/sandbox/Toread/tools&&python getMyPyxidaNC.py>vnclog 2>&1&
-cd /home/goettingen_wgd/sandbox/Toread/tools&&python rttCollector.py>rttlog 2>&1&
